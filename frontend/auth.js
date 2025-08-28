cd0787efc463061de8af951a13064e63a87385b8
Lakshit0510 <sachdevalakshit2005@gmail.com>
2025-08-28 21:12:37 +0530

    connecting to backend

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000"; 
+const API_BASE_URL = "https://finance-tracker-lbql.onrender.com/"; 
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
 const errorMessage = document.getElementById("error-message");
@@ -60,4 +60,5 @@ if (registerForm) {
             errorMessage.textContent = error.message;
         }
     });
+
 }
