87c87babdcc80a7dcfd7ba354f341b912de4b2c2
Lakshit0510 <sachdevalakshit2005@gmail.com>
2025-10-05 23:24:13 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -172,7 +172,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         <span class="transaction-info">${tx.time}</span>
                     </div>
                     <div class="transaction-amount ${isExpense ? 'expense' : 'income'}">
-                        ${isExpense ? '-' : '+'}$${Math.abs(tx.amount).toFixed(2)}
+                        ${isExpense ? '-' : '+'}&#8377;${Math.abs(tx.amount).toFixed(2)}
                     </div>
                     <button class="delete-tx-btn" data-id="${tx.id}" title="Delete Transaction">&times;</button>
                 `;
@@ -252,6 +252,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+
 
 
 
